604408ada3bfe6c65fdbaab43089c2083d3464fd
lgrgurev <88197513+lgrgurev@users.noreply.github.com>
2021-11-29 16:02:16 -0300

    pp
    pp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <NavBar/>
         </a>
       </header>
     </div>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,32 @@
+//import React from 'react'
+import { Nav, Navbar, Container, NavDropdown} from "react-bootstrap"
+
+const NavBar = () => {
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Inicio</Nav.Link>
+                        <Nav.Link href="#secos">Secos</Nav.Link>
+                        <Nav.Link href="#chacinados">Chacinados</Nav.Link>
+                        <NavDropdown title="Nuestros Productos" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#Jamones">Jamones</NavDropdown.Item>
+                        <NavDropdown.Item href="#Salames">Salames</NavDropdown.Item>
+                        <NavDropdown.Item href="#Embutidos">Embutidos</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#Otros">Otros</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>   
+        
+        </div>
+    )
+}
+
+export default NavBar
